2c21710039a880ecdaeef0c5603950c429fac574
roehrle208 <roehrle208@gmail.com>
2023-10-04 15:36:49 +0200

    ELUNA_LOG_* handling gefixed

--- CMangos/GlobalMethods.h
+++ CMangos/GlobalMethods.h
@@ -320,10 +320,10 @@ namespace LuaGlobalFunctions
      * Low GUID is an ID to distinct the objects of the same type.
      *
      * [Player] and [Creature] for example can have the same low GUID but not GUID.
-     * 
+     *
      * On TrinityCore all low GUIDs are different for all objects of the same type.
      * For example creatures in instances are assigned new GUIDs when the Map is created.
-     * 
+     *
      * On MaNGOS and cMaNGOS low GUIDs are unique only on the same map.
      * For example creatures in instances use the same low GUID assigned for that spawn in the database.
      * This is why to identify a creature you have to know the instanceId and low GUID. See [Map:GetIntstanceId]
@@ -2025,7 +2025,7 @@ namespace LuaGlobalFunctions
 
     /**
      * Adds a taxi path to a specified map, returns the used pathId.
-     * 
+     *
      * Note that the first taxi point needs to be near the player when he starts the taxi path.
      * The function should also be used only **once** per path added so use it on server startup for example.
      *
@@ -2303,7 +2303,7 @@ namespace LuaGlobalFunctions
      */
     int PrintInfo(lua_State* L)
     {
-        ELUNA_LOG_INFO("%s", GetStackAsString(L).c_str());
+        ELUNA_LOG_INFO("{}", GetStackAsString(L).c_str());
         return 0;
     }
 
@@ -2314,7 +2314,7 @@ namespace LuaGlobalFunctions
      */
     int PrintError(lua_State* L)
     {
-        ELUNA_LOG_ERROR("%s", GetStackAsString(L).c_str());
+        ELUNA_LOG_ERROR("{}", GetStackAsString(L).c_str());
         return 0;
     }
 
@@ -2325,7 +2325,7 @@ namespace LuaGlobalFunctions
      */
     int PrintDebug(lua_State* L)
     {
-        ELUNA_LOG_DEBUG("%s", GetStackAsString(L).c_str());
+        ELUNA_LOG_DEBUG("{}", GetStackAsString(L).c_str());
         return 0;
     }
 
@@ -2946,7 +2946,7 @@ namespace LuaGlobalFunctions
 
         return 0;
     }
-    
+
     luaL_Reg GlobalMethods[] =
     {
         // Hooks
